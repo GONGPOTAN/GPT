a35d0a6ae9084db4da5a4a54de469e781da491de
Gongpotan <wndnjs1907@naver.com>
2025-04-12 20:06:42 +0900

    fix: fully define ThemeProviderProps to fix build

--- frontend-dashboard/src/components/theme-provider.tsx
+++ frontend-dashboard/src/components/theme-provider.tsx
@@ -3,14 +3,17 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: "class" | "style" | Array<"class" | "style">;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
+
 export function ThemeProvider({
   children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <NextThemesProvider attribute="class">
-      {children}
-    </NextThemesProvider>
-  );
+  ...props
+}: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
